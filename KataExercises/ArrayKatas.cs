19a4816d3650447645ba7c72de90a129455cefca
[MarcusQ] <marcusquigley@gmail.com>
2019-03-21 11:20:07 -0400

    some arrays

--- KataExercises/ArrayKatas.cs
+++ KataExercises/ArrayKatas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,13 +17,54 @@ namespace KataExercises
             List<int> results = new List<int>();
             foreach (var item in values)
             {
-                if (n% item == 0)
+                if (item % n == 0)
                 {
                     results.Add(item);
                 }
             }
-            
             return results.ToArray();
+        }
+
+        public static int[] NumbersDivisibleByNCleaner(int[] values, int n)
+        {
+            if (values == null) throw new ArgumentNullException("values");
+            return values.Where(v => v % n == 0).ToArray();
+        }
+        public static string HighestAndLowestNumbers(string values)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            var numberStrings = values.Split(' ');
+            for (int i = 0; i < numberStrings.Length; i++)
+            {
+                var number = int.Parse(numberStrings[i]);
+                if (i == 0)
+                {
+                    min = number;
+                    max = number;
+                }
+                else
+                {
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                    else if (number > max)
+                    {
+                        max = number;
+                    }
+                }
+            }
+
+            return $"{max} {min}";
+        }
+
+        public static string HighestAndLowestNumbersCleaner(string values)
+        {
+            //  if (string.IsNullOrEmpty(values)) return new int[] { };
+            var numbers = values.Split(' ')
+                                .Select(s => int.Parse(s));
+            return $"{numbers.Max()} {numbers.Min() }";
         }
 
         //check if an array of integers contains two elements that sum to a given x
@@ -30,6 +72,72 @@ namespace KataExercises
         {
 
 
+        }
+
+        //Sum of the first nth term of Series
+        //Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +
+        public static string SeriesSum(int n)
+        {
+            if (n == 0) return "0.00";
+            double sum = 1.00;
+            for (int i = 1; i < n; i++)
+            {
+                //var subSum = 1.0 / (i * 3 + 1);
+                sum += 1.0 / (i * 3 + 1);
+            }
+            return sum.ToString("0.00", CultureInfo.InvariantCulture);
+        }
+        //return (from i in Enumerable.Range(0, n) select 1.0 / (3 * i + 1)).Sum().ToString("0.00");
+        public static string SeriesSumCleaner(int n)
+        {
+            return Enumerable.Range(0, n)
+                   .Select(i => 1f / (i * 3 + 1))
+                   .Sum()
+                   .ToString("0.00");
+        }
+
+        public static int Persistence(long n)
+        {
+            if (n.ToString().Length == 1) return 0;
+            int result = 0;
+
+            Stack<long> numbers = SplitNumbers(n);
+            while (numbers.Count > 1)
+            {
+                long sum = 1;
+                result += 1;
+                foreach (var item in numbers)
+                {
+                    sum *= item;
+                }
+                numbers = SplitNumbers(sum);
+            }
+            return result;
+        }
+
+        static Stack<long> SplitNumbers(long n)
+        {
+            Stack<long> numbers = new Stack<long>();
+            while (n > 0)
+            {
+                numbers.Push(n % 10);
+                n = n / 10;
+            }
+            return numbers;
+        }
+
+        public static int PersistenceClean(long n)
+        {
+            int count = 0;
+            while (n > 9)
+            {
+                count += 1;
+                n = n.ToString()
+                     .Select(digit => int.Parse(digit.ToString()))
+                     .Aggregate((x, y) => x * y);
+            }
+
+            return count;
         }
     }
 }
--- KataExercisesTests/ArrayKatasTests.cs
+++ KataExercisesTests/ArrayKatasTests.cs
@@ -5,15 +5,88 @@ using Xunit;
 
 namespace KataExercisesTests
 {
-    public class UnitTest1
+    public class ArrayKatasTests
     {
         [Theory]
         [InlineData(new int[] { 7, 1, 3, 2, 4, 5, 6 }, 2)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, 2)]
         public void AreNumbersDivisibleByN(int[] values, int n)
         {
             var actual = ArrayKatas.NumbersDivisibleByN(values, n);
             var expected = new int[] { 2, 4, 6 };
             Assert.Equal(expected, actual);
         }
+
+        [Theory]
+        [InlineData(new int[] { 7, 1, 3, 2, 4, 5, 6 }, 2)]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, 6 }, 2)]
+        public void AreNumbersDivisibleByNCleaner(int[] values, int n)
+        {
+            var actual = ArrayKatas.NumbersDivisibleByNCleaner(values, n);
+            var expected = new int[] { 2, 4, 6 };
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("7 1 3 2 4 5 6", "7 1")]
+        [InlineData("1 2 3 4 5 6", "6 1")]
+        [InlineData("1 2 2 -15 -6", "2 -15")]
+        public void ReturnHighestAndLowestNumbers(string values, string expected)
+        {
+            var actual = ArrayKatas.HighestAndLowestNumbers(values);
+             Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("7 1 3 2 4 5 6", "7 1")]
+        [InlineData("1 2 3 4 5 6", "6 1")]
+        [InlineData("1 2 2 -15 -6", "2 -15")]
+        public void ReturnHighestAndLowestNumbersCleaner(string values, string expected)
+        {
+            var actual = ArrayKatas.HighestAndLowestNumbersCleaner(values);
+            Assert.Equal(expected, actual);
+        }
+
+         [Theory]
+         [InlineData(1,"1.00")]
+         [InlineData(2, "1.25")]
+         [InlineData(5, "1.57")]
+        public void SeriesSum(int n, string expected)
+        {
+            var actual = ArrayKatas.SeriesSum(n);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(1, "1.00")]
+        [InlineData(2, "1.25")]
+        [InlineData(5, "1.57")]
+        public void SeriesSumCleaner(int n, string expected)
+        {
+            var actual = ArrayKatas.SeriesSumCleaner(n);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(999, 4)]
+        [InlineData(39, 3)]
+        [InlineData(4, 0)]
+        public void Persistence(int n, int expected)
+        {
+            var actual = ArrayKatas.Persistence(n);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(999, 4)]
+        [InlineData(39, 3)]
+        [InlineData(4, 0)]
+        public void PersistenceClean(int n, int expected)
+        {
+            var actual = ArrayKatas.PersistenceClean(n);
+            Assert.Equal(expected, actual);
+        }
+
+        
     }
 }
