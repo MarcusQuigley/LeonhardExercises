0a3b426bb11d007d59a48178e5043cdbb4a64293
[MarcusQ] <marcusquigley@gmail.com>
2019-03-21 07:31:06 -0400

    Adding katas for Data structures

---
+++ KataExercises/ArrayKatas.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataExercises
+{
+    public class ArrayKatas
+    {
+        //Find numbers which are divisible by given number
+        public static int[] NumbersDivisibleByN(int[] values, int n)
+        {
+            if (values == null) throw new ArgumentNullException("values");
+
+            List<int> results = new List<int>();
+            foreach (var item in values)
+            {
+                if (n% item == 0)
+                {
+                    results.Add(item);
+                }
+            }
+            
+            return results.ToArray();
+        }
+
+        //check if an array of integers contains two elements that sum to a given x
+        public static void HasElementsThatSum(int[] values, int n)
+        {
+
+
+        }
+    }
+}
---
+++ KataExercisesTests/ArrayKatasTests.cs
@@ -0,0 +1,19 @@
+﻿using KataExercises;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace KataExercisesTests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(new int[] { 7, 1, 3, 2, 4, 5, 6 }, 2)]
+        public void AreNumbersDivisibleByN(int[] values, int n)
+        {
+            var actual = ArrayKatas.NumbersDivisibleByN(values, n);
+            var expected = new int[] { 2, 4, 6 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
